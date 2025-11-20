2233777afcd849c7f6e04f15623902bc89937fcd
Sachin Pal <sachinpal9352@gmial.com>
2025-11-20 19:18:39 +0530

    first commits12112s

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Baba – Your Luck Lottery Number",
+  title: "Baba Satta Matka",
   icons: {
     icon: "/icon.jpg"
   },
